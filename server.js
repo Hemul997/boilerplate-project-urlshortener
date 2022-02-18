628675960dd42aa5f3308b6be2af8440ae8a7b1c
Hemul997 <nikitamoshkin1997@gmail.com>
2022-02-18 11:29:15 +0300

    Set up middleware for body-parsing.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
+var bodyParser = require('body-parser');
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,9 +13,11 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
@@ -21,4 +26,10 @@ app.get('/api/hello', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+app.post('/api/shorturl', function(req, res) {
+  res.json({
+    'url': req.body['url']
+  });
 });
